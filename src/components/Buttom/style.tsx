f2d2e4532215bdb7786cfed98491ff9e664f9e8a
TaiLinh <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2022-10-20 08:28:10 +0700

    clear backage

--- src/components/Buttom/style.tsx
+++ src/components/Buttom/style.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-
+  button: {
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 5,
+    marginBottom: 5,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'rgb(1, 147, 134)',
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    borderRightWidth: 1,
+    borderLeftWidth: 1,
     
-
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
 
 export default styles;
--- src/components/Input/style.tsx
+++ src/components/Input/style.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-
-    lable: {
-        color: "rgb(1, 147, 134)",
-        fontSize: 16,
-        fontWeight: "bold",
-    },
-    textInput: {
-        borderBottomWidth: 1,
+    container: {
+      marginBottom: 20
+  },
+  lable: {
+    color: 'rgb(1, 147, 134)',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  textInput: {
+    borderBottomWidth: 1,
     height: 50,
-    borderColor: "gray",
-    },
-
-
+    borderColor: 'gray',
+  },
 });
 
 export default styles;
